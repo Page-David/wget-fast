8c4ae41e5b1271f4c7abbedcf4a95735df282996
Page David <David991010@gmail.com>
2016-07-23 19:26:56 +0800

    Dont start thread when no 206 response

--- HTTP_Downloader.py
+++ HTTP_Downloader.py
@@ -94,7 +94,8 @@ class Downloader(object):
             self.num_speed[self.worker_count - 1] = self.total_speed
             if (self.num_speed[self.worker_count - 1] >= self.num_speed[self.worker_count - 2]
                     and self.worker_count <= self.configer.max_thread 
-                    and not self.configer.down_queue.empty()):
+                    and not self.configer.down_queue.empty()
+                    and self.configer.partital_content):
                 t = threading.Thread(target = self._download)
                 self.thread_list.append(t)
                 t.start()
